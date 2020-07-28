7b49f2ee19f119ec7c51e0796f76ded204daaef0
Mike Matera <maximus@fatboycentral.com>
2020-07-28 16:39:28 -0700

    A long day of lesson refactoring.

--- p4e/drawing.py
+++ p4e/drawing.py
@@ -15,19 +15,25 @@ from IPython.display import display
 class Turtle:
 
     def __init__(self, size=(400,400)):
+        self._size = size
         self._canvas = MultiCanvas(width=size[0], height=size[1])
-        self._current = (size[0]//2, size[1]//2)
+        self._turtle = Image.from_file(pathlib.Path(__file__).parent / "turtle.png")
+        self.clear()
+        
+    def clear(self):
+        """Clear the canvas and start over."""
+        self._canvas.clear()
+        self._current = (self._size[0]//2, self._size[1]//2)
         self._cur_heading = (3 * math.pi) / 2 # in Canvas Y is negative.
         self._pendown = True 
         self._show = True       
-        self._turtle = Image.from_file(pathlib.Path(__file__).parent / "turtle.png")
         self._draw_turtle()
-
+        
     def _draw_turtle(self):
         """Update the position of the turtle."""
         with hold_canvas(self._canvas[2]):
-            self._canvas[2].reset_transform()
             self._canvas[2].clear()
+            self._canvas[2].reset_transform()
             if self._show:
                 self._canvas[2].translate(self._current[0], self._current[1])
                 self._canvas[2].rotate(self._cur_heading - (3 * math.pi) / 2)
@@ -54,8 +60,8 @@ class Turtle:
     def goto(self, x, y):
         """Goto a point in the coordinate space."""
         start = self._current
-        self._current = (x, y)
-        if self.pendown:
+        self._current = (self._size[0]//2 + x, self._size[1]//2 - y)
+        if self._pendown:
             self._canvas[1].begin_path()
             self._canvas[1].move_to(*start)
             self._canvas[1].line_to(*self._current)
@@ -87,19 +93,17 @@ class Turtle:
         """Show the turtle in the scene.""" 
         self._show = True 
         self._draw_turtle()
-        return self._canvas
 
     def hide(self):
         """Hide the turtle in the scene.""" 
         self._show = False 
         self._draw_turtle()
-        return self._canvas
 
     def background(self, filename):
         """Set a background image.""" 
         img = Image.from_file(filename)
         self._canvas[0].draw_image(img, x=0, y=0, \
             width=self._canvas[0].size[0], height=self._canvas[0].size[1])
-            
+          
     def _ipython_display_(self):
         display(self._canvas)
