d4d37878eaa95c4433926ef4c6b9e969fd14f13c
Mike Matera <matera@lifealgorithmic.com>
2021-12-14 22:04:56 -0800

    Better turtle.

--- Packages/p4e/widgets.py
+++ Packages/p4e/widgets.py
@@ -4,45 +4,42 @@ Factory methods for widgets.
 Author: Mike Matera
 """
 
-from typing import Iterable
-
+import sys 
 import ipywidgets
 
+from typing import Dict, Iterable
 
-def selector(description: str, values: Iterable):
-    """
-    Create a selection widget
-    """    
-    return ipywidgets.Select(
-        options=list(values),
-        description=description,
-    )
+from IPython.core.display import display 
 
-def textbox():
+def layout(*rows: Iterable[ipywidgets.Widget]) -> ipywidgets.Widget:
     """
-    Create a text selection input
+    Create a simple grid layout. 
     """
+    display(ipywidgets.VBox(tuple(map(ipywidgets.HBox, rows))))
 
+def bind(fname: str, widgets: Dict[str, ipywidgets.Widget]) -> ipywidgets.Output:
+    """
+    Bind controls to a function. 
+    """
+    def wrapper(**kwargs):
+        main = sys.modules['__main__']
+        if hasattr(main, fname):
+            got = getattr(main, fname)(**kwargs)
+            if isinstance(got, tuple):
+                display(*got)
+            else:
+                display(got)
+        else:
+            display(f"Function __main__.{fname} is not defined.")
 
-def colorpicker():
-    """
-    Pick a color
-    """
-
-
-def datepicker():
-    """
-    Pick a date
-    """
-
-
-def intslider():
-    """
-    Integer slider
-    """
-
-
-def floatslider():
-    """
-    Floating point number
-    """
+    out = ipywidgets.interactive_output(wrapper, widgets)
+    label = ipywidgets.HTML(f"""<pre style="">{fname}</pre>""")
+    panel = ipywidgets.VBox([label, out])
+    panel.layout = {
+        'margin': '1em',
+    }    
+    out.layout = {
+        'border': '1px solid lightgrey',
+        'padding': '1em 1em 1em 1em',
+    }
+    return panel
